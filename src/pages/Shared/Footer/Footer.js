8a28416e84454dadbd9af03d517b9d2412fca0aa
farid5630 <mdforidulislam5630@gmail.com>
2022-05-16 23:12:48 +0600

    footer complete

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-            <h3 className='text-success bg-dark py-5'>This is Footer</h3>
-        </div>
+      <div className="bg-dark py-5">
+            <div className='w-75 mx-auto text-center'>
+                <Link className='text-decoration-none text-muted me-3' to='/'>Home</Link>
+                <Link className='text-decoration-none text-muted me-3' to='/about'>About Us</Link>
+                <Link className='text-decoration-none text-muted me-3' to='/blogs'>Blogs</Link>
+                <Link className='text-decoration-none text-muted me-3' to='/login'>Sign in</Link>
+            </div>
+            <hr className='text-white w-75 mx-auto text-bold'/>
+      </div>
     );
 };
 
